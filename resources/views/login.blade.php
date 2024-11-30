f74f09ff527214dbd2560548f06762b913e89f75
Juan Manuel <JMVZ010802@GMAIL.COM>
2024-11-30 01:13:04 -0700

    Se agrego la config para las vistas de css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,10 +2,37 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login | Ludiflex</title>
+    @vite('resources/css/styleLogin.css')
+
 </head>
 <body>
-    <h2> HELLOOO WORD</h2>
+    <div class="login-box">
+        <div class="login-header">
+            <header>Login</header>
+        </div>
+        <div class="input-box">
+            <input type="text" class="input-field" placeholder="Usuario" autocomplete="off" required>
+        </div>
+        <div class="input-box">
+            <input type="password" class="input-field" placeholder="Password" autocomplete="off" required>
+        </div>
+        <div class="forgot">
+            <section>
+                <input type="checkbox" id="check">
+                <label for="check">Recuerdame</label>
+            </section>
+            <section>
+                
+            </section>
+        </div>
+        <div class="input-submit">
+            <button class="submit-btn" id="submit"></button>
+            <label for="submit">Entrar</label>
+        </div>
+       
+    </div>
 </body>
 </html>
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,16 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: [
+                'resources/js/app.js',
+                'resources/css/app.css', 
+                'resources/css/styleLogin.css',  
+                'resources/css/inicio-EA.css',
+                'resources/css/styleInicio-EA.css',
+                'resources/css/styleInicio-JC.css',
+                'resources/css/styleInfoHectarea.css',
+                'resources/css/styleIngresaCaja-EA.css',
+            ],
         }),
     ],
 });
