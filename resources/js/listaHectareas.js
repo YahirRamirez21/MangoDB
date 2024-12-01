661228b9f77099f40328b775f81e37a8a7b6496f
XimenaLH <ximena.hernandez@triagons.com>
2024-11-30 18:21:44 -0700

    Nuevas vistas y archivos css

---
+++ resources/js/listaHectareas.js
@@ -0,0 +1,24 @@
+console.log('LEGGOOO')
+const hectareas = [
+    {id: 1, nombre: 'Hectarea 1 ', location: 'Culiacan'},
+    {id: 2, nombre: 'Hectarea 2 ', location: 'Mochis'},
+    {id: 3, nombre: 'Hectarea 3 ', location: 'Sanalona'},
+    {id: 4, nombre: 'Hectarea 4 ', location: 'Sinaloa Leyva'},
+    {id: 5, nombre: 'Hectarea 5 ', location: 'Choix'},
+    {id: 6, nombre: 'Hectarea 6 ', location: 'Navolato'},
+    {id: 7, nombre: 'Hectarea 7 ', location: 'Culiacan'},
+]
+
+const listaHectareas = document.getElementById("sectionListaHectareas")
+
+if (listaHectareas) {
+    hectareas.forEach((hectarea) => {
+        const div = document.createElement("div")
+        div.textContent = `${hectarea.nombre} - ${hectarea.location}`
+        div.className = "itemHectarea"
+        listaHectareas.appendChild(div)
+    });
+}
+
+
+
---
+++ resources/views/inicio-JC.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio Jefe Cuadrilla</title>
+    <link rel="stylesheet" >
+    @vite(['resources/css/styleInicio-JC.css', 'resources/js/listaHectareas.js', 'resources/css/style-components/styleComponents.css'])
+
+</head>
+<body>
+    <x-menu/>
+    <main>
+        <x-inputBusqueda/>
+        <section id="sectionListaHectareas">
+
+        </section>
+
+    </main>
+</body>
+</html>
