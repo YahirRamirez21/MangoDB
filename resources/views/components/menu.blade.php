ce9b0c2aa664d6feff68fbba1bfb9490846eef77
Yahir Ramirez <jesusyahir178@gmail.com>
2024-12-06 00:22:40 -0700

    Agg iconos

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,14 +1,20 @@
 <header>
     <nav class="navbar" role="navigation" aria-label="Navegación principal">
         <a href="#home"><img src="{{ asset('img/logo.png') }}" class="logo"></a>
-        <a id="inicio" href="{{ $link ?? '#' }}">Inicio</a>
+        <a id="inicio" href="{{ $link ?? '#' }}">
+            <svg class="home" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+            </svg>
+            <span>Inicio</span>
+
+        </a>
         <form action="{{ route('logout') }}" method="POST" style="display: inline;">
             @csrf
             <div class="contenedor-close">
-                <button type="submit" class="logout close"> 
+                <button type="submit" class="logout close">
                     <svg class="icono" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
-                    </svg> 
+                    </svg>
                     <span>Cerrar Sesion</span>
                 </button>
             </div>
--- resources/views/inicioJC.blade.php
+++ resources/views/inicioJC.blade.php
@@ -10,48 +10,56 @@
 </head>
 
 <body>
-    
-    <x-menu link="{{ url('/inicioHectarea') }}"/>
+
+    <x-menu link="{{ url('/inicioHectarea') }}" />
 
 
     <main>
         @if($hectareas->isEmpty())
-            <p>No tienes hectáreas asignadas.</p>
+        <p>No tienes hectáreas asignadas.</p>
         @else
-            <!-- Formulario de Filtro -->
-            <form method="GET" action="{{ route('hectareas.filtrar') }}">
-                <section class="contenedor_inputs">
-                    <div class="inputs">
-                        <label><strong>Seleccione la opción para filtrar:</strong></label>
-                        <label>
-                            <input type="radio" name="tipo" value="autorizada" {{ old('tipo') == 'autorizada' ? 'checked' : '' }}>
-                            Autorizada
-                        </label>
-                        <label>
-                            <input type="radio" name="tipo" value="no_autorizada" {{ old('tipo') == 'no_autorizada' ? 'checked' : '' }}>
-                            No Autorizada
-                        </label>
-                    </div>
-                </section>
-            </form>
+        <!-- Formulario de Filtro -->
+        <form method="GET" action="{{ route('hectareas.filtrar') }}">
+            <section class="contenedor_inputs">
+                <div class="inputs">
+                    <label><strong>Seleccione la opción para filtrar:</strong></label>
+                    <label>
+                        <input type="radio" name="tipo" value="autorizada" {{ old('tipo') == 'autorizada' ? 'checked' : '' }}>
+                        Autorizada
+                        <svg class="autorizar" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                            <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                        </svg>
 
-            <!-- Lista de Hectáreas -->
-            <div class="listaHectareas">
-                @foreach($hectareas as $hectarea)
-                    <div class="itemHectarea">
-                        <!-- Imagen Representativa -->
-                        <img src="{{ $hectarea->imagen_url ?? 'https://img.freepik.com/fotos-premium/hay-muchos-arboles-mango-campo_941033-8221.jpg?w=1060' }}" alt="Imagen de Hectárea">
+                    </label>
+                    <label>
+                        <input type="radio" name="tipo" value="no_autorizada" {{ old('tipo') == 'no_autorizada' ? 'checked' : '' }}>
+                        No Autorizada
+                        <svg class="no_autorizar" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                            <path fill-rule="evenodd" d="m6.72 5.66 11.62 11.62A8.25 8.25 0 0 0 6.72 5.66Zm10.56 12.68L5.66 6.72a8.25 8.25 0 0 0 11.62 11.62ZM5.105 5.106c3.807-3.808 9.98-3.808 13.788 0 3.808 3.807 3.808 9.98 0 13.788-3.807 3.808-9.98 3.808-13.788 0-3.808-3.807-3.808-9.98 0-13.788Z" clip-rule="evenodd" />
+                        </svg>
 
-                        <!-- Información de la Hectárea -->
-                        <strong>Hectárea ID:</strong> {{ $hectarea->id ?? 'Sin ID' }}
-                        <div class="info">
-                            <strong>Renta:</strong> {{ $hectarea->renta ?? 'Sin renta' }}<br>
-                            <strong>Porcentaje:</strong> {{ $hectarea->porcentaje_general ?? 'Sin Porcentaje' }}
-                        </div>
-                        <a class="botonVerInfo" href="{{ route('hectareas.info', $hectarea->id) }}">Ver Información</a>
-                    </div>
-                @endforeach 
+                    </label>
+                </div>
+            </section>
+        </form>
+
+        <!-- Lista de Hectáreas -->
+        <div class="listaHectareas">
+            @foreach($hectareas as $hectarea)
+            <div class="itemHectarea">
+                <!-- Imagen Representativa -->
+                <img src="{{ $hectarea->imagen_url ?? 'https://img.freepik.com/fotos-premium/hay-muchos-arboles-mango-campo_941033-8221.jpg?w=1060' }}" alt="Imagen de Hectárea">
+
+                <!-- Información de la Hectárea -->
+                <strong>Hectárea ID:</strong> {{ $hectarea->id ?? 'Sin ID' }}
+                <div class="info">
+                    <strong>Renta:</strong> {{ $hectarea->renta ?? 'Sin renta' }}<br>
+                    <strong>Porcentaje:</strong> {{ $hectarea->porcentaje_general ?? 'Sin Porcentaje' }}
+                </div>
+                <a class="botonVerInfo" href="{{ route('hectareas.info', $hectarea->id) }}">Ver Información</a>
             </div>
+            @endforeach
+        </div>
         @endif
     </main>
 
